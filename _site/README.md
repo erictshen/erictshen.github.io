b8d8b9a2a4f560b36fa9789b62423df8db551c52
Eric Shen <email.ericshen@gmail.com>
2016-08-28 20:21:00 -0700

    Removed _site from repo

--- _site/README.md
+++
@@ -1,3 +0,0 @@
-# erictshen.github.io
-Eric Shen's personal website.
-Live at <a href="http://ericshen.com/">http://ericshen.com//</a>
--- _site/js/website.js
+++
@@ -1,8 +0,0 @@
-angular.module('website', []).
-  config(function($routeProvider) {
-    $routeProvider.
-      when('/', {templateUrl:'home.html'}).
-      // when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
-      // when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
-      otherwise({redirectTo:'/'});
-  });
